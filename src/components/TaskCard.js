103359c56d1c4ffe3f058ce2fd4026a04ca64473
Proteen Das <proteen.das@olamnet.com>
2024-04-10 11:24:59 +0530

    updates

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function TaskCard(props) {
+function TaskCard({ id, task, handleDelete }) {
   return (
-    <li key={props.task.id} className={props.task.completed ? "completed" : "incomplete"}>
-        <span>{props.task.id} - {props.task.name}</span>
-        <button onClick={() => props.handleDelete(props.task.id)} className='delete'>Delete</button>
+    <li className={task.completed ? "completed" : "incomplete"}>
+        <span>{id} - {task.name}</span>
+        <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
     </li>
   )
 }
--- src/pages/TaskList.js
+++ src/pages/TaskList.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './TaskList.css';
 import TaskCard from '../components/TaskCard';
 import { useTitle } from '../hooks/useTitle';
+import { BoxCard } from '../components/BoxCard';
 
 export const TaskList = (props) => {
     useTitle("Task List");
@@ -25,9 +26,21 @@ export const TaskList = (props) => {
                 <ul>
                     <button onClick={() => setShow(!show)} className="trigger">Toggle</button>
                     { show && tasks.map((task) => (
-                        <TaskCard task={task} handleDelete={handleDelete}/>
+                        <TaskCard key={task.id} task={task} handleDelete={handleDelete}/>
                     ))}
                 </ul>
+                <BoxCard result="success">
+                    <p className="title">Lorem ipsum dolor sit amet.</p>
+                    <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, similique!</p>
+                </BoxCard>
+                <BoxCard result="warning">
+                    <p className="title">Lorem, ipsum dolor.</p>
+                    <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                </BoxCard>
+                <BoxCard result="alert">
+                    <p className="title">Lorem, ipsum.</p>
+                    <p className="description">Lorem ipsum dolor sit amet.</p>
+                </BoxCard>
             </div>
         </>
     )
